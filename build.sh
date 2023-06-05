d4b58760171a233273ae61c4f75b886f77818af6
sulincix <parduscix@yandex.ru>
2023-06-05 20:41:46 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -120,11 +120,13 @@ echo "nameserver 1.1.1.1" > work/rootfs/etc/resolv.conf
 mkdir -p work/rootfs/lib/modules/
 cp -rvf work/firmware-master/modules/* work/rootfs/lib/modules/
 chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "apk update"
-chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "apk add kmod networkmanager eudev dbus openrc shadow openssh "
+chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "apk add kmod networkmanager eudev dbus openrc shadow openssh"
 chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "rc-update add dbus"
 chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "rc-update add networkmanager"
 chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "rc-update add udev"
 chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "rc-update add sshd"
+chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "rc-update add swclock"
+chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "rc-update add hwclock"
 #### create default user
 chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "useradd user -m -U"
 echo -e "alpine\nalpine\n" | chroot work/rootfs /usr/bin/qemu-aarch64-static /bin/ash -c "passwd root"
